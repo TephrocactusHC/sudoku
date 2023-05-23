062964a3e66a02c7f2e2f0735e438f5c9c2eb6c4
myc <1531176958@qq.com>
2023-05-23 15:26:17 +0800

    fix all bugs

--- README.md
+++ README.md
@@ -2,4 +2,24 @@
 Generate sudoku problems &amp; solve sudoku problems
 # TODO
 - [x] Differents Ends
-- [ ] Input through command line parameters
+- [x] Input through command line parameters
+# Usage
+You can use the following command:
+```
+cargo run -- -n <num> -m <num> -s
+```
+Among them, <num> is the number you want to specify for generating the quantity of problems and unknown numbers. The -s parameter indicates solving the problem. You can also use the -h parameter to display help information.
+
+
+For example, if you want to generate 5 questions with 15 unknown numbers in each question and solve them, you can run the following command:
+```
+cargo run -- -n 5 -m 15 -s
+```
+The program will generate Sudoku puzzles and solve them based on the parameters you provide.
+
+For more, you can use -h for help:
+```
+cargo run -- -h
+```
+# More
+In fact, I haven't delved into the mathematical principles behind the Sudoku game, such as why there are only 36 final solutions in situations that are asymmetric and non-repetitive. Therefore, I don't know what problems generating a large number of final solutions will cause. This project is for entertainment purposes only. Due to the use of backtracking algorithms, performance may be very poor once the quantity becomes large.
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,62 @@
 mod generate;
 mod solve;
+use std::env;
 
 fn main() {
-    generate::generate_sudoku(10, 20); // Generate 10 puzzles with 20 unknowns each
-    solve::solve_sudoku_puzzles();
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        print_help();
+        return;
+    }
+
+    let mut num_puzzles = 10;
+    let mut num_unknowns = 20;
+    let mut solve_puzzles = false;
+
+    for i in 1..args.len() {
+        match args[i].as_str() {
+            "-n" => {
+                if i + 1 < args.len() {
+                    num_puzzles = parse_arg(&args[i + 1], num_puzzles);
+                }
+            }
+            "-m" => {
+                if i + 1 < args.len() {
+                    num_unknowns = parse_arg(&args[i + 1], num_unknowns);
+                }
+            }
+            "-s" => {
+                solve_puzzles = true;
+            }
+            "-h" => {
+                print_help();
+                return;
+            }
+            _ => {}
+        }
+    }
+
+    generate::generate_sudoku(num_puzzles, num_unknowns);
+
+    if solve_puzzles {
+        solve::solve_sudoku_puzzles();
+    }
+}
+
+fn parse_arg(arg: &str, default: usize) -> usize {
+    match arg.parse::<usize>() {
+        Ok(value) => value,
+        Err(_) => default,
+    }
+}
+
+fn print_help() {
+    println!("Usage: program_name [options]");
+    println!("Options:");
+    println!("-n <num>       Number of puzzles to generate (default: 10)");
+    println!("-m <num>       Number of unknowns in each puzzle (default: 20)");
+    println!("-s             Solve the generated puzzles");
+    println!("-h             Display this help message");
 }
 
